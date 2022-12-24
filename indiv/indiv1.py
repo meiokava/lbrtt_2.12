085b4617ae710306b75d4524fac1441fb433522d
meiokava <dragonmeix5@gmail.com>
2022-12-24 10:50:25 +0300

    completed tasks

---
+++ indiv/indiv1.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def external(start=0):
+    def middle(func):
+        def inner(*args):
+            arg = args[0]
+            return func(arg) + start
+        return inner
+    return middle
+
+
+@external(start=5)
+def sum_line(string):
+    return sum(list(map(int, string.split())))
+
+
+if __name__ == "__main__":
+    numb = input('Please enter numbers without gaps: ')
+    print(sum_line(numb))
